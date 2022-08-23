9a92fc9cba244a81412333fefe202aa5839cb7aa
KuznetsovaE <kettt56@mail.ru>
2022-08-24 02:42:19 +0300

    QQQ

---
+++ _Seminar 1/Zadanie 2.cs
@@ -0,0 +1,74 @@
+﻿//1. Напишите программу, которая на вход принимает два числа и проверяет,
+//   является ли первое число квадратом второго.
+//a = 5; b = 25 -> да
+//a = 2 b = 10 -> нет
+//a = 9; b = -3 -> нет
+//a = -3 b = 9 -> да
+
+// Console.Write("Введите целое число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());//converttoint32 переводит "строку" в "число". Ридлайн принимает значение стринг, ей нужно показать что наше число не стринг, а инт
+// Console.Write("Введите целое число b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a * a == b)
+// {
+//     Console.WriteLine("Да!");
+// }
+
+// else if (b * b == a)
+// {
+//     Console.WriteLine("Да!");
+// }
+
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+// закомментировать - ctrl+k+c
+
+
+
+
+
+
+
+//2. Напишите программу, которая будет выдавать название дня недели
+//   по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+
+
+// Console.Write("Введите целое число от 1 до 7: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if(a == 1) Console.WriteLine("Понедельник");
+
+// else if(a == 2) Console.WriteLine("Вторник");
+
+// else if(a == 3) Console.WriteLine("Среда");
+
+// else if(a == 4) Console.WriteLine("Четверг");
+
+// else if(a == 5) Console.WriteLine("Пятница");
+
+// else if(a == 6) Console.WriteLine("Суббота");
+
+// else if(a ==7) Console.WriteLine("Воскресенье");
+
+// else Console.WriteLine("Нет такого дня недели");
+
+
+// 3. Программа которая проверяет при вводе трех чисел, не является ли какое-либо число произведенеим двух других
+
+Console.Write("Введите целое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ( a == b *c || b == a * c || c == a * b) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
---
+++ _Seminar 1/Zadanie 3.cs
@@ -0,0 +1,60 @@
+﻿            //цикл вайл. пусть вводит число и+1 пока не станет и равно 15
+
+// Console.Write ("Введите целое число ");
+// int i = 5;
+// while (i <= 15)
+// {
+//     Console.Write(i+", ");
+//     i++;                            //i++ это = i+1
+// }
+
+
+            // 5. Напишите программу, которая на вход принимает 
+            //одно число (N), а на выходе показывает все целые 
+            //числа в промежутке от -N до N.
+            // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+            // 2 -> " -2, -1, 0, 1, 2"
+
+
+// Console.Write ("Введите целое число ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = -N; i <= N ; i++) Console.Write(i + ", ");
+
+
+
+
+            // . Напишите программу, которая принимает на вход трёхзначное 
+            //число и на выходе показывает последнюю цифру этого числа.
+            // 456 -> 6
+            // 782 -> 2
+            // 918 -> 8
+
+// Console.Write ("Введите целое число ");
+// int  N = Convert.ToInt32(Console.ReadLine());
+// int a = N % 10;
+// Console.WriteLine(a);
+
+
+            // При вводе четырехзначного числа выводится 3я цифра
+
+// Console.Write ("Введите целое число ");
+// int  N = Convert.ToInt32(Console.ReadLine());
+// int a = N / 10;
+// a = a % 10;
+// Console.WriteLine(a);
+
+
+Console.Write ("Введите целое число ");
+string? N_str = Console.ReadLine();
+int lnght = N_str.Length;
+int  N = Convert.ToInt32(N_str);
+int i = 0;
+
+while (i < (lnght - 2))
+{
+    N = N/10;
+    i++;
+}
+
+Console.WriteLine(N % 10);
