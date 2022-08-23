87cd03c9644fbb7bfa2bc4eaa6dba824fc0590c5
KuznetsovaE <kettt56@mail.ru>
2022-08-23 21:30:24 +0300

    Random

--- Div/Program.cs
+++ Div/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿double numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA / numberB);
