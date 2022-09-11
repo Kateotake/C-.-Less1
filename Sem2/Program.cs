928adfbb3482b06c50cad2e643f9ee2c45fc8d15
KuznetsovaE <kettt56@mail.ru>
2022-09-11 22:16:02 +0300

    add sem

---
+++ Sem2/Program.cs
@@ -0,0 +1,127 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// void PrintMaxDigit()
+// {
+//     int x = new Random().Next(10 ,100); //генерируем случайное число от 10 до 99. пишем справа 100 потому что правая граница не входит в зону
+//     Console.WriteLine($"сгенерировалась случайное число {x}"); // $ - переформатирование в текст
+//     int x1 = x / 10;
+//     int x2 = x % 10;
+//     if (x1 > x2) Console.WriteLine (x1);
+//     else Console.WriteLine (x2);
+// }
+
+// PrintMaxDigit();
+// PrintMaxDigit();
+
+
+// void PrintMaxDigitArg(int xx)
+// {
+//    // Console.WriteLine($"сгенерировалась случайное число {xx}"); // $ - переформатирование в текст
+//     int x1 = xx / 10;
+//     int x2 = xx % 10;
+//     if (x1 > x2) Console.WriteLine (x1);
+//     else Console.WriteLine (x2);
+// }
+
+// PrintMaxDigitArg(int.Parse(Console.ReadLine())); // сразу информацию вводимое с клавы преобразую в целое число и передаю на вход
+
+// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+// 1 уровень - обернуть в функцию, 
+// 2 уровень - чтоб удалялась вторая цифра числа любой разрядности
+
+// void Example()
+
+    // {
+        //int x = new Random().Next(100 ,1000); //генерируем случайное число от 100 до 999. пишем справа 1000 потому что правая граница не входит в зону
+        // Console.WriteLine($"Случайное трехзначное число {x}"); // $ - переформатирование в текст
+        // // int x1 = x / 100;
+        // // int x2 = x1 * 10;
+        // // int x3 = x % 10;
+        // // int x4 = x2 + x3;
+        // // Console.WriteLine (x4);
+
+        // // другой вариант
+
+        // int a = x / 100;
+        // int b = x % 10;
+        // Console.WriteLine ($"{a}{b}"); //если написать консольврайт то не надо переводить через $ 
+        //($ - это сигнал комплилятору что сейчас будут значения переменных, и потом в фигурных скобках мы прописываем переменные
+    //}
+   
+//Example();
+
+// функция ретерн -"отвечает" программе ответ. Получит
+
+// string Example (int x)
+// {
+//      int a = x / 100;
+//      int b = x % 10;
+//      return $"{a}{b}"; 
+
+// }
+// int x = new Random().Next(100,1000);
+// Console.WriteLine($"сгенерировалось число {x}");
+
+// Console.WriteLine(Example(x)+ " moved kuda-to");
+
+            // 12. Напишите программу, которая будет принимать на вход два числа и выводить, 
+            //является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+            // 34, 5 -> не кратно, остаток 4
+            // 16, 4 -> кратно
+
+// Console.WriteLine ("Введите 1 число");
+// int a = Convert.ToInt32 (Console.ReadLine ());
+// Console.WriteLine ("Введите 2 число");
+// int b = Convert.ToInt32 (Console.ReadLine ());
+// if (a > b)
+// {
+//     if (a % b == 0) Console.WriteLine($"{b} кратно {a}");
+//     else Console.WriteLine ($"не кратно, остаток {a % b}");
+// }
+// else if (b > a)
+// {
+//     if (b % a == 0) Console.WriteLine($"{a} кратно {b}");
+//     else Console.WriteLine ($"не кратно, остаток {b % a}");
+// }
+
+            // то же самое задание, через ретерн
+
+// string PrintOst()
+// {
+//     Console.WriteLine ("Введите 1 число");
+//     int a = Convert.ToInt32 (Console.ReadLine ());
+//     Console.WriteLine ("Введите 2 число");
+//     int b = Convert.ToInt32 (Console.ReadLine ());
+
+//     int ostat = a % b;
+//     if (ostat == 0) return "кратно";
+//     else return $"не кратно, остаток {ostat}";
+// }
+// Console.WriteLine (PrintOst());
+
+
+            // тоже самое задание через воид
+
+void Kratnoe(int a, int b)
+{
+    if (a % b == 0) Console.WriteLine($"{b} кратно {a}");
+    else Console.WriteLine ($"не кратно, остаток {a % b}");
+}
+
+void Print(int x, int y)
+{
+    if (y > x) Kratnoe (y,x);
+    else Kratnoe(x,y);
+}
+
+int xx = Convert.ToInt32(Console.ReadLine()); //parse  то же самое что и convert.toint32
+int yy = Convert.ToInt32(Console.ReadLine());
+
+Print(xx,yy);
+Print(xx,yy);
---
+++ Sem3/Program.cs
@@ -0,0 +1,261 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+// try{   //попытаться сделать это:
+//     Console.Write ("введите координату х ");
+//     int x = int.Parse (Console.ReadLine());
+//     Console.Write ("введите координату y ");
+//     int y = int.Parse (Console.ReadLine());
+
+//     int res;
+//     if (x > 0 && y > 0) res = 1;
+//     else if (x > 0 && y < 0) res = 2;
+//     else if (x < 0 && y < 0) res = 3;
+//     else if (x < 0 && y > 0) res = 3;
+//     else res = 0;
+
+//     if ( res != 0) Console.WriteLine($"это четверть с номером {res}");
+//     else Console.WriteLine("точка лежит на осях");
+// }
+// catch // а если не получится, то "поймать"
+// {
+//     Console.WriteLine ("Надо было написать именно целые числа");
+// }
+
+// с функцией
+
+// string GetNum4()
+// {
+//     Console.Write ("введите координату х ");
+//     int x = int.Parse (Console.ReadLine());
+//     Console.Write ("введите координату y ");
+//     int y = int.Parse (Console.ReadLine());
+
+//     int res;
+//     if (x > 0 && y > 0) res = 1;
+//     else if (x > 0 && y < 0) res = 2;
+//     else if (x < 0 && y < 0) res = 3;
+//     else if (x < 0 && y > 0) res = 4;
+//     else res = 0;
+
+//     if ( res != 0) return($"это четверть с номером {res}"); // != не равно
+//     else return("точка лежит на осях");
+// }
+// try
+// { 
+//    Console.WriteLine (GetNum4());
+// }
+// catch // а если не получится, то "поймать"
+// {
+//     Console.WriteLine ("Надо было написать именно целые числа");
+// }
+
+//_____________________________________________________________________________________________________________
+
+// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// string GetNum4()
+// {
+//     Console.Write ("Введите номер четверти ");
+//     int k = int.Parse (Console.ReadLine());
+  
+//     if ( k == 1)  return ("x > 0  y > 0");
+//     else if  (k == 2) return ("x > 0  y < 0");
+//     else if  (k == 3) return ("x < 0  y < 0");
+//     else if  (k == 4) return ("x < 0  y > 0");
+
+//     else return("такой четверти нет");
+// }
+// try
+// { 
+//    Console.WriteLine (GetNum4());
+// }
+// catch // а если не получится, то "поймать"
+// {
+//     Console.WriteLine ("Надо было написать именно целые числа");
+// }
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+//  string GetNum4()
+// {
+//     Console.Write ("введите координату x Точки А ");
+//     double x1 = Convert.ToDouble (Console.ReadLine());
+//     Console.Write ("введите координату y Точки А ");
+//     double y1 = Convert.ToDouble (Console.ReadLine());
+//     Console.Write ("введите координату x Точки B ");
+//     double x2 = Convert.ToDouble (Console.ReadLine());
+//     Console.Write ("введите координату y Точки В ");
+//     double y2 = Convert.ToDouble (Console.ReadLine());
+
+//     double res = Math.Sqrt (Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2));
+
+//     return $"{res}";
+// }
+// try
+// { 
+//    Console.WriteLine (GetNum4());
+// }
+// catch // а если не получится, то "поймать"
+// {
+//     Console.WriteLine ("Надо было написать именно целые числа");
+// }
+
+
+
+// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// string GetNum4()
+// {
+//     Console.Write ("введите целое число N ");
+//     int N = Convert.ToInt32(Console.ReadLine());
+//     int k = 1;
+//     string res = string.Empty;
+
+//     while (k <= N)
+//     {  
+//         res = res + k*k + ", ";
+//         k++;
+//     }
+// return res;
+// }    
+// try
+// { 
+//    Console.WriteLine (GetNum4());
+// }
+// catch 
+// {
+//     Console.WriteLine ("Надо было написать именно целые числа");
+// }
+
+// без функции
+//  Console.Write ("введите целое число N ");
+//     int N = Convert.ToInt32(Console.ReadLine());
+//     int k = 0;
+//      while (k <= N)
+//     {  
+//          int res = k*k*k;
+//          Console.Write ($"{res}, ");
+//          k++;
+//     }     
+
+
+// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+//  Console.Write ("введите пятизначное число N: ");
+//     string N = Console.ReadLine();
+//     int dlina = N.Length;
+//     int K = Convert.ToInt32 (N);
+//     int n1 = K/10000;
+//     int n2 = (K/1000) % 10;
+//     int n4 = (K/10) % 10;
+//     int n5 = K % 10;
+
+// if (dlina == 5)
+// {
+//     if ((n1 == n5) && (n2 == n4)) Console.WriteLine("Да");
+//     else Console.WriteLine ("Нет");
+// }
+// else Console.WriteLine ("Нужно пятизначное число");
+
+
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+    // Console.Write ("введите координату x Точки А ");
+    // double x1 = Convert.ToDouble (Console.ReadLine());
+    // Console.Write ("введите координату y Точки А ");
+    // double y1 = Convert.ToDouble (Console.ReadLine());
+    // Console.Write ("введите координату z Точки А ");
+    // double z1 = Convert.ToDouble (Console.ReadLine());
+    // Console.Write ("введите координату x Точки B ");
+    // double x2 = Convert.ToDouble (Console.ReadLine());
+    // Console.Write ("введите координату y Точки В ");
+    // double y2 = Convert.ToDouble (Console.ReadLine());
+    // Console.Write ("введите координату z Точки B ");
+    // double z2 = Convert.ToDouble (Console.ReadLine());
+
+    // double res = Math.Sqrt (Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2) + Math.Pow((z1 - z2),2));
+
+    // Console.WriteLine(res);
+
+// try
+// { 
+//    Console.WriteLine (GetNum4());
+// }
+// catch // а если не получится, то "поймать"
+// {
+//     Console.WriteLine ("Надо было написать именно целые числа");
+// }
+
+
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+//  Console.Write ("введите целое число N ");
+//     int N = Convert.ToInt32(Console.ReadLine());
+//     int k = 0;
+//      while (k <= N)
+//     {  
+//          int res = k*k*k;
+//          Console.Write ($"{res}, ");
+//          k++;
+//     }     
+
+
+// задача необязательная
+
+// Напишите программу, которая считывает с консоли числа (по одному в строке) 
+//до тех пор, пока сумма введённых чисел не будет равна 0 и сразу после этого выводит сумму квадратов всех считанных чисел.
+// Гарантируется, что в какой-то момент сумма введённых чисел окажется равной 0, после этого считывание продолжать не нужно.
+
+// В примере мы считываем числа 1, -3, 5, -6, -10, 13; в этот момент замечаем, 
+//что сумма этих чисел равна нулю и выводим сумму их квадратов, не обращая внимания на то, что остались ещё не прочитанные значения.﻿
+
+// На входе:
+// 1
+// -3
+// 5
+// -6
+// -10
+// 13
+// 4
+// -8
+
+// На выходе:
+
+// 340
+
+
+int sum = 0;
+int sqr = 0;
+while (true)
+{
+    int N = Convert.ToInt32(Console.ReadLine());
+    sum = sum + N;
+    sqr = sqr + N*N;
+  
+if (sum ==0) break;
+}
+//  while (Console.ReadKey().Key != ConsoleKey.Escape) {
+//         }
+Console.WriteLine (sqr);
